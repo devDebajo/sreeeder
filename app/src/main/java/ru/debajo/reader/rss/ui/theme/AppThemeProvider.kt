e4113a9b36b33aa0f2e26c4105343e6a3c934ed9
debajo <nikitosbobin@gmail.com>
2021-12-06 21:26:12 +0500

    перевел на английский поднял версию починил баг с динамической темой

--- app/src/main/java/ru/debajo/reader/rss/ui/theme/AppThemeProvider.kt
+++ app/src/main/java/ru/debajo/reader/rss/ui/theme/AppThemeProvider.kt
@@ -1,8 +1,8 @@
 package ru.debajo.reader.rss.ui.theme
 
 import android.content.SharedPreferences
+import android.os.Build
 import androidx.core.content.edit
-import com.google.android.material.color.DynamicColors
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -61,7 +61,7 @@ class AppThemeProvider(
         currentAppThemeConfigMutable.emit(AppThemeConfig(mode, dynamicTheme))
     }
 
-    fun supportDynamicTheme(): Boolean = DynamicColors.isDynamicColorAvailable()
+    fun supportDynamicTheme(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
 
     private companion object {
         const val THEME_KEY = "current_theme"
