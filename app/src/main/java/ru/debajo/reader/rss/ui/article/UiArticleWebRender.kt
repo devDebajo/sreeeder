6327c54f1779a8c351fac1392a6166b98f0c4c8a
debajo <nikitosbobin@gmail.com>
2022-01-14 23:17:09 +0500

    added placeholder

--- app/src/main/java/ru/debajo/reader/rss/ui/article/UiArticleWebRender.kt
+++ app/src/main/java/ru/debajo/reader/rss/ui/article/UiArticleWebRender.kt
@@ -1,6 +1,7 @@
 package ru.debajo.reader.rss.ui.article
 
 import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
@@ -14,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.stringResource
@@ -133,6 +135,20 @@ fun UiArticleWebRender(
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun LongPullContainer(key: Any?, content: @Composable () -> Unit) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .pointerInput(key) {
+                detectDragGestures { change, dragAmount ->
+
+                }
+            }) {
+        content()
     }
 }
 
